7c7bd3560a81d06211989a05f559a1ba05923455
mcstitzer <mcstitzer@ucdavis.edu>
2018-07-28 14:06:29 -0500

    Update compare_mtec.sh

--- mtec/compare_mtec.sh
+++ mtec/compare_mtec.sh
@@ -7,5 +7,5 @@ USEARCH=/home/mstitzer/software/vsearch/bin/vsearch
 
 srun -p bigmemh --time=1-00:00 ${USEARCH} -allpairs_global MTEC_TIR.fa -blast6out MTEC_TIR.allvall.out -id 0.8 -query_cov 0.8 -target_cov 0.8 --threads 1
 
-
-srun -p bigmemh --time=1-00:00 ${SILIX} MTEC_TIR.fa MTEC_TIR.allvall.out -f MTEC -i 0.8 -r 0.8 --net > MTEC_TIR.8080.fnodes
+## having trouble with boost libraries, so using silix 1.2.9
+~/software/silix-1.2.9/src/silix  MTEC_TIR.fa MTEC_TIR.allvall.out -f MTEC -i 0.8 -r 0.8 --net > MTEC_TIR.8080.fnodes
